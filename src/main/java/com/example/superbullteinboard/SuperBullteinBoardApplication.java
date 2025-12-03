113a71f42757179065e86a55c8551da5a041ca78
KimSummerrain <doeun457@gmail.com>
2025-12-03 14:45:08 +0900

    Initial Commit Spring Project

---
+++ src/main/java/com/example/superbullteinboard/SuperBullteinBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.superbullteinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBullteinBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperBullteinBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/superbullteinboard/SuperBullteinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superbullteinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBullteinBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
